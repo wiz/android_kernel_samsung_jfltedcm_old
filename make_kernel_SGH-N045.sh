2df8e8c3f91aeb5804e869267190be7fb9b08d0d
J. Maurice <j@wiz.biz>
2013-05-26 03:12:11 +0900

    Add compilation script

--- make_kernel_SGH-N045.sh
+++ make_kernel_SGH-N045.sh
@@ -1,3 +1,5 @@
+export CROSS_COMPILE=/home/jmaurice/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
 export VARIANT_DEFCONFIG=jf_dcm_defconfig 
 make jf_defconfig
-make
+make -j16
+make zImage
